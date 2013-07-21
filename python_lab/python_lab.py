592dad76ce1f9b861b2f056d61ffbf1b51729957
Andrew Magill <andrewbmagill@yahoo.com>
2013-07-21 01:33:38 -0500

    finished!

--- python_lab/python_lab.py
+++ python_lab/python_lab.py
@@ -78,36 +78,36 @@ value_list = [x[k] for x in dlist]
 ## Task 22
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
 k = 'Bilbo'
-value_list_modified_1 = ['Ian','Martin'] # <-- Use the same expression here
+value_list_modified_1 = [x[k] if k in x else 'NOT PRESENT' for x in dlist] # <-- Use the same expression here
 k = 'Frodo'
-value_list_modified_2 = ['Elijah','NOT PRESENT'] # <-- as you do here
+value_list_modified_2 = [x[k] if k in x else 'NOT PRESENT' for x in dlist] # <-- as you do here
 
 ## Task 23
-square_dict = {...}
+square_dict = {x:x**2 for x in range(100)}
 
 ## Task 24
 D = {'red','white','blue'}
-identity_dict = {...}
+identity_dict = {x:x for x in D}
 
 ## Task 25
 base = 10
 digits = set(range(10))
-representation_dict = {...}
+representation_dict = {(x*base**0 + y*base**1 + z*base**2):[z,y,x] for x in digits for y in digits for z in digits}
 
 ## Task 26
 d = {0:1000.0, 1:1200.50, 2:990}
 names = ['Larry', 'Curly', 'Moe']
-listdict2dict = { ... }
+listdict2dict = {names[x]:y for (x,y) in d.items()}
 
 ## Task 27
-def nextInts(L): return [ ... ]
+def nextInts(L): return [x+1 for x in L]
 
 ## Task 28
-def cubes(L): return [ ... ] 
+def cubes(L): return [x**3 for x in L] 
 
 ## Task 29
-def dict2list(dct, keylist): return [ ... ]
+def dict2list(dct, keylist): return [dct[x] for x in keylist]
 
 ## Task 30 
-def list2dict(L, keylist): return { ... } 
+def list2dict(L, keylist): return {keylist[x]:L[x] for x in range(len(keylist))} 
 
